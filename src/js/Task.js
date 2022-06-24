488a619186f179a79326d934d675e22de4407eaa
Kaminsky <kaminskyfrontend@gmail.com>
2022-06-24 07:12:45 +0300

    type val

--- src/js/Task.js
+++ src/js/Task.js
@@ -2,7 +2,7 @@ class Task {
     #id;
     #title;
     #count;
-    constructor(id = '', title = '', count = 0) {
+    constructor(id, title, count) {
         this.#id = id;
         this.#title = title;
         this.#count = count;
@@ -14,7 +14,7 @@ class Task {
 
     set setIncrementCount(count) {
         count++
-        this.#count = count
+        this.#count = Number(count)
     }
 
     get getTitleVal() {
@@ -22,11 +22,25 @@ class Task {
     }
 
     set setTitleVal(title) {
-        this.#title = title
+        this.#title = String(title)
+    }
+
+    get getId() {
+        return console.log(this.#id)
+    }
+
+    set setIdVal(id) {
+        this.#id = String(id)
     }
 }
 
 const task = new Task()
 
-// task.setIncrementCount = 0;
-// task.getIncrementCount;
+task.setIncrementCount = 0;
+task.getIncrementCount;
+
+task.setTitleVal = 1234
+task.getTitleVal
+
+task.setIdVal = 12349987
+task.getId
